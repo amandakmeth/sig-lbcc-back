8b37566f22a8718a0b848a017d45754d2f6c67ee
amandinha951 <amandinha951@gmail.com>
2026-04-23 17:30:21 -0300

    autencticação e +

--- src/modules/usuarios/services/usuarios.service.js
+++ src/modules/usuarios/services/usuarios.service.js
@@ -1,48 +1,57 @@
 import supabase from '../../../config/supabase.js'
+import bcrypt from 'bcrypt'
 
-//LISTAR
+// LISTAR
 export const listarUsuarios = async () => {
-return await supabase
-    .from('usuarios')
-    .select('*')
+    return await supabase
+        .from('usuarios')
+        .select('id, nome, email, perfil, ativo, created_at')
 }
 
-//BUSCAR POR ID
+// BUSCAR POR ID
 export const buscarUsuarioPorId = async (id) => {
-return await supabase
-    .from('usuarios')
-    .select('*')
-    .eq('id', id)
-    .single()
+    return await supabase
+        .from('usuarios')
+        .select('id, nome, email, perfil, ativo, created_at')
+        .eq('id', id)
+        .single()
 }
 
-//CRIAR
-export const inserirUsuario = async ({ nome, email, perfil }) => {
+// CRIAR
+export const inserirUsuario = async ({ nome, email, perfil, senha }) => {
+    const senhaHash = await bcrypt.hash(senha, 10)
+
 return await supabase
     .from('usuarios')
     .insert([
     {
         nome,
         email,
-        perfil: perfil || 'operador'
+        perfil: perfil || 'operador',
+        senha: senhaHash
     }
     ])
-    .select()
+    .select('id, nome, email, perfil, ativo, created_at')
 }
 
-//ATUALIZAR
+// ATUALIZAR
 export const atualizarUsuario = async (id, dados) => {
+
+if (dados.senha) {
+    dados.senha = await bcrypt.hash(dados.senha, 10)
+}
+
 return await supabase
     .from('usuarios')
     .update(dados)
     .eq('id', id)
-    .select()
+    .select('id, nome, email, perfil, ativo, created_at')
 }
 
-//DELETAR 
+// DELETAR
 export const deletarUsuario = async (id) => {
-return await supabase
-    .from('usuarios')
-    .delete()
-    .eq('id', id)
+    return await supabase
+        .from('usuarios')
+        .delete()
+        .eq('id', id)
 }
