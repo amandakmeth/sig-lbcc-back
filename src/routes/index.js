5ede55776fd22d6332d03a6941dc70adf6197c40
amandinha951 <amandinha951@gmail.com>
2026-04-24 21:46:30 -0300

    feat: areas de atendimento

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import usuariosRoutes from '../modules/usuarios/routes/usuarios.routes.js'
 import authRoutes from '../modules/auth/routes/auth.routes.js'
+import areasRoutes from '../modules/areas/routes/areas.routes.js'
+
 
 const router = express.Router()
 
 router.use('/usuarios', usuariosRoutes)
 router.use('/auth', authRoutes)
+router.use('/areas', areasRoutes)
 
 router.get('/', (req, res) => {
     res.json({ status: 'API rodando' })
